a019c425d5c56998a1138331bf8ef23b99fc0a46
Minkyu SHIM <hello.mkshim@gmail.com>
2025-12-07 15:35:29 +0100

    connect database using docker

--- src/main/java/com/epita/airlineapi/model/User.java
+++ src/main/java/com/epita/airlineapi/model/User.java
@@ -1,14 +1,12 @@
 package com.epita.airlineapi.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
