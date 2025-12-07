dd410a908d297ced91d30e9bb4a876a7a0e4de57
Minkyu SHIM <hello.mkshim@gmail.com>
2025-12-07 21:56:51 +0100

    airport model, controller, service and repository

--- src/main/java/com/epita/airlineapi/service/UserService.java
+++ src/main/java/com/epita/airlineapi/service/UserService.java
@@ -3,7 +3,6 @@ package com.epita.airlineapi.service;
 import com.epita.airlineapi.model.User;
 import com.epita.airlineapi.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,7 +14,6 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    @Autowired
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
